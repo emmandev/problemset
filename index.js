51d297b225f70052f2fc4c677c6b3cceba7c5f2b
Emmanuel Tierra <emman.tierra@gmail.com>
2020-05-29 16:14:55 +0800

    feat(doAsync): solve problem requirements

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const { asyncOp } = require("./lib/lib")
  * Each element in the array can be either of type String or [String].
  * @param {array} arr
  */
-const doAsync = async (arr = []) => {}
+const doAsync = async (arr = []) => {
+  for (element of arr) {
+    if (Array.isArray(element)) {
+      await Promise.all(element.map((item) => asyncOp(item)))
+    } else {
+      await asyncOp(element)
+    }
+  }
+}
 
 const input = ["A", ["B", "C", "D", "E"], "F", "G", ["H", "I"]]
 doAsync(input)
