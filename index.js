5d735b606b8f8218e04872888b91b7f80a0bd825
Emmanuel Tierra <emman.tierra@gmail.com>
2020-05-29 16:11:27 +0800

    feat(doAsync): set problem requirements

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // solutions here
+
+const { asyncOp } = require("./lib/lib")
+
+//region 1. Asynchronous Operations
+/**
+ * Accepts an array arr as input.
+ * Each element in the array can be either of type String or [String].
+ * @param {array} arr
+ */
+const doAsync = async (arr = []) => {}
+
+const input = ["A", ["B", "C", "D", "E"], "F", "G", ["H", "I"]]
+doAsync(input)
+//endregion 1. Asynchronous Operations
